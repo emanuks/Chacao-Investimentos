9837d5ea878c47a00b49f45c0dab1a118bb1cf3a
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-11-14 23:03:31 -0300

    :card_file_box: Adding phone to user

--- chacao-investimentos/app/models/user.rb
+++ chacao-investimentos/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :phones, dependent: :destroy
+  accepts_nested_attributes_for :phones
 end
--- chacao-investimentos/db/schema.rb
+++ chacao-investimentos/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_013943) do
+ActiveRecord::Schema.define(version: 2020_11_15_020000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "phones", force: :cascade do |t|
+    t.string "ddd"
+    t.string "number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_phones_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_11_15_013943) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "phones", "users"
 end
