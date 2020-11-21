aadeae71530c14aea59dd48b04682da82b7c3df4
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-11-21 20:34:05 -0300

    Adicionando Modal

--- chacao-investimentos/config/routes.rb
+++ chacao-investimentos/config/routes.rb
@@ -9,11 +9,14 @@ Rails.application.routes.draw do
 
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
-  delete '/logout', to: 'user_sessions#destroy', as: :logout
+  get '/logout', to: 'user_sessions#destroy', as: :logout
   
   get '/products', to: 'products#index', as: :products
 
   get '/direct_treasures/:id', to: 'direct_treasures#show', as: :show_direct_treasure
   get '/fixed_incomes/:id', to: 'fixed_incomes#show', as: :show_fixed_income
   get '/investment_funds/:id', to: 'investment_funds#show', as: :show_investment_fund
+  post '/application_fis/new', to: 'application_fis#create'
+  post '/application_dts/new', to: 'application_dts#create'
+  post '/application_ifs/new', to: 'application_ifs#create'
 end
--- chacao-investimentos/db/seeds.rb
+++ chacao-investimentos/db/seeds.rb
@@ -8,3 +8,12 @@
 
 AccountType.create(name: 'Conta Corrente')
 User.create(cpf: '92482292042', email: 'admin@admin.com', password: '1234')
+
+# Renda Fixa
+FixedIncome.create(category: "CDB", entity: "Daycoval Digital", due_date: 24, tax: 0.01, time_limit: "20:00", value: 75)
+
+# Fundo de Investimento
+InvestmentFund.create(category: "Cambial", redemption_period: "D + 4", name: "Daycoval Títulos Públicos III FIRF", value: 25)
+
+# Tesouro Direto
+DirectTreasure.create(name: "Banco do Brasil", index: "IPCA", due_date: "2020-11-20", tax: 0.02, value: 20)
